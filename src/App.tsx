8283657c0fa5a5cea120d6d2113b831786fe5cb9
GenXTechdotCode <142334403+GenXTechdotCode@users.noreply.github.com>
2023-09-23 23:23:55 +0800

    Learning Advance Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 function App() {
+	const personName = {
+		first: 'Bruce',
+		last: 'Wayne',
+	}
+	const nameList = [ 
+		{
+			first: 'Bruce',
+			last: 'Wayne'
+		},
+		{
+			first: 'Clark',
+			last: 'Kent'
+		},
+		{
+			first: 'Princess',
+			last: 'Diana'
+		}
+	]
 	return (
 		<div className="App">
-			<Greet name='Vishwas' />
+			<Greet name='Vishwas' messageCount={20} isLoggedIn={false} />
+			<Person name={personName} />
+			<PersonList names={nameList} />
+			<Status status='loading'/>
+			<Heading>Placeholder text</Heading>
+			<Oscar>
+				<Heading>Oscar goes to Leonardo Dicarpio!</Heading>
+			</Oscar>
 		</div>
-	);
+	)
 }
 
 export default App;
